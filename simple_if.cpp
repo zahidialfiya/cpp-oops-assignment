cbcfa1bbdd031d1fc5d0eafca3f795fd61b67f05
zahidialfiya <zahidialfiya2006@gmail.com>
2026-01-02 21:51:08 +0530

    Add files via upload

---
+++ simple_if.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+
+    // Taking input from user
+    cout << "Enter a number: ";
+    cin >> number;
+
+    // Using simple if statement
+    if (number > 0)
+    {
+        cout << "The number is positive." << endl;
+    }
+
+    return 0;
+}
