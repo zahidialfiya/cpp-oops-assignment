5a8186a1a0eebff341bf2b2afd2ce7ba6eca9c2e
zahidialfiya <zahidialfiya2006@gmail.com>
2026-01-11 21:30:33 +0530

    Add files via upload

---
+++ cin_cout_example.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    float marks;
+
+    // Taking input using cin
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "Enter your marks: ";
+    cin >> marks;
+
+    // Displaying output using cout
+    cout << "\nYour Details:" << endl;
+    cout << "Age: " << age << endl;
+    cout << "Marks: " << marks << endl;
+
+    return 0;
+}
